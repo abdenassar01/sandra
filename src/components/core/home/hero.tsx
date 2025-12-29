8329da90be7c65af412930ca38a4fbb14d6abd90
abdenassar01 <abdenassaramimi99@gmail.com>
2025-12-29 22:54:07 +0100

    feat: update and add various images for UI components and modify reviews database.

--- src/components/core/home/hero.tsx
+++ src/components/core/home/hero.tsx
@@ -8,7 +8,7 @@ export async function HeroSection() {
 	return (
 		<section id="#Home" className="relative h-[calc(100svh-120px)] rounded-2xl overflow-hidden">
 			<div className="absolute inset-0">
-				<div className="w-full h-full bg-[url('/images/hero.webp')] bg-cover bg-center bg-no-repeat" />
+				<div className="w-full h-full bg-[url('/images/hero.jpeg')] bg-cover bg-center bg-no-repeat" />
 				<div className="absolute inset-0 bg-linear-to-b from-black/70 via-black/50 to-[rgba(34,43,139,0.3)]"></div>
 				<div className="absolute inset-0 bg-linear-to-t from-[rgba(144,172,238,0.2)] to-transparent"></div>
 			</div>
